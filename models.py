672d3e2888f06ad9eac3f3d0c89b6159f9ee16a4
sacred70 <sacred70@mail.ru>
2023-03-26 15:17:04 +0700

    models

---
+++ models.py
@@ -0,0 +1,13 @@
+from marshmallow import Schema, fields, validate
+# проверка на правильность команд от пользователя
+# список дозволенных команд
+
+VALID_CMD_COMMANDS = ('filter', 'unique', 'map', 'sort')
+
+
+class RequestSchema(Schema):
+    file_name = fields.Str(required=True)
+    cmd1 = fields.Str(required=True, validate=validate.OneOf(VALID_CMD_COMMANDS))
+    value1 = fields.Str(required=True)
+    cmd2 = fields.Str(required=True, validate=validate.OneOf(VALID_CMD_COMMANDS))
+    value2 = fields.Str(required=True)
